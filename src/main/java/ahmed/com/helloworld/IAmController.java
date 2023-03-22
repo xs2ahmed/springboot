341b3f9f44fd040192aeb48a9eefbe429a8f0bf7
mahmed <opensource@mahmed.net>
2023-03-23 01:12:21 +0500

    Initial commit

---
+++ src/main/java/ahmed/com/helloworld/IAmController.java
@@ -0,0 +1,16 @@
+package mahmed.net.helloworld;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class IAmController {
+
+    @GetMapping("/hello")
+    public String hello()
+    {
+       System.out.println("Hello there...hello I got hit but not in the debugger");
+       return "Hello World";
+    }
+}
---
+++ src/test/java/ahmed/com/helloworld/HttpResponseTest.java
@@ -0,0 +1,28 @@
+package mahmed.net.helloworld;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.web.client.RestTemplate;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class HttpResponseTest {
+    @LocalServerPort
+    int port;
+    private static RestTemplate restTemplate;
+    @BeforeAll
+    public static void init() {
+
+        restTemplate = new RestTemplate();
+    }
+
+    @Test
+    public void shouldPassIfStringMatches()
+    {
+        String result = restTemplate.getForObject("http://localhost:"+port+"/hello",String.class);
+        assertEquals("Hello World", result);
+    }
+}
